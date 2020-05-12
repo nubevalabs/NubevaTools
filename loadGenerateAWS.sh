505b65045ce92c55d3b7382d86374d494589eb38
Erik Freeland <erik.freeland@gmail.com>
2020-05-12 11:20:13 -0700

    Update loadGenerateAWS.sh
    Add support for local web server.

--- loadGenerateAWS.sh
+++ loadGenerateAWS.sh
@@ -8,6 +8,7 @@ NAME=
 
 while true; do
 #Grab 100MB file on S3
-wget -q -O /dev/null https://nubevalabs.s3.amazonaws.com/100MB.zip --no-check-certificate
+curl -s -O /dev/null https://nubevalabs.com/100MB.zip
+#wget -q -O /dev/null https://nubevalabs.s3.amazonaws.com/100MB.zip --no-check-certificate
 sleep $[ ( $RANDOM % 5 )  + 1 ]s
 done
