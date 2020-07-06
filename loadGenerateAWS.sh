b3831fdaf3a674ca3bd1bb27a30f5bc761fedf7e
Erik Freeland <ejfree@Eriks-MacBook-Pro-3.local>
2020-07-06 09:30:27 -0700

    back to http

--- loadGenerateAWS.sh
+++ loadGenerateAWS.sh
@@ -8,8 +8,8 @@ NAME=
 
 while true; do
 #Grab 100MB file on S3
-curl -s -O /dev/null http://nubevalabs.com/100MB.zip
-#curl -s -O /dev/null https://nubevalabs.com/100MB.zip
+#curl -s -O /dev/null http://nubevalabs.com/100MB.zip
+curl -s -O /dev/null https://nubevalabs.com/100MB.zip
 
 #wget -q -O /dev/null https://nubevalabs.s3.amazonaws.com/100MB.zip --no-check-certificate
 
