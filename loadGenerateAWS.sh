b2ee02939aa7875582766fa54897fa317ac621a9
Erik Freeland <ejfree@Eriks-MacBook-Pro-3.local>
2020-07-07 16:17:33 -0700

    Add 1MB file

--- loadGenerateAWS.sh
+++ loadGenerateAWS.sh
@@ -15,8 +15,8 @@ while true; do
 
 #Grab 1MB file on S3
 #curl -s -O /dev/null http://nubevalabs.com/1MB.zip
-#curl -s -O /dev/null https://nubevalabs.com/1MB.zip
-curl -s -O /dev/null https://nubevalabs.s3.amazonaws.com/1MB.zip
+#curl -s -k -O /dev/null https://nubevalabs.com/1MB.zip
+curl -s -k -O /dev/null https://nubevalabs.s3.amazonaws.com/1MB.zip
 
 
 sleep $[ ( $RANDOM % 1 )  + 1 ]s
